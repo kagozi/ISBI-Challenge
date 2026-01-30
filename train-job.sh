6af64bd1e9d2ece805d43f9327181d8adab58a60
kagozi <alexkagozi@gmail.com>
2026-01-30 01:05:50 -0600

    ++ training script

---
+++ train-job.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# GPU job submission script for SLURM
+# Stage 1: RGB-only Video-to-Gloss Training
+#
+
+### Job Configuration #########################################################
+
+#SBATCH --job-name=isbi-stage1-train
+#SBATCH --output=isbi-stage1-train-%j.out
+
+#SBATCH --get-user-env
+
+# Resources
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+
+# Time limit
+#SBATCH --time=48:00:00
+
+# Email notifications
+#SBATCH --mail-user=alex.kagozi@coyotes.usd.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+##############################################################################
+
+nvidia-smi
+
+# ---------------------------------------------------------------------------
+# Activate environment
+# ---------------------------------------------------------------------------
+
+eval "$(mamba shell hook --shell bash)"
+mamba activate slt-multistream
+
+
+python main.py
+
+nvidia-smi
