2d8f18a907590fca14d60b573e6081f23cdcaa78
kagozi <alexkagozi@gmail.com>
2026-02-02 17:22:25 -0600

    ++ ADD IMPORT

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 from tqdm import tqdm
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.optim.lr_scheduler import CosineAnnealingLR, ReduceLROnPlateau
 from torch.utils.data import Dataset, DataLoader
