b8be467291cdff35cf6d07ae5614197243fe1e07
kagozi <alexkagozi@gmail.com>
2026-02-22 15:46:52 -0600

    ++ resolve warning

--- jobs/train-job_second.sh
+++ jobs/train-job_second.sh
@@ -3,8 +3,8 @@
 # GPU job submission script for SLURM
 ### Job Configuration #########################################################
 
-#SBATCH --job-name=efficientnet-torchstain-train
-#SBATCH --output=efficientnet-torchstain-%j.out
+#SBATCH --job-name=swin-torchstain-train
+#SBATCH --output=swin-torchstain-%j.out
 
 #SBATCH --get-user-env
 
